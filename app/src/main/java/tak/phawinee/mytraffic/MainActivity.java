87637d948cf5f884cbc930c54e1090143b40a245
phawinee.tak <phawinee.tak@gmamil.com>
2016-07-31 10:59:20 +0700

    Intent ti Webview

--- app/src/main/java/tak/phawinee/mytraffic/MainActivity.java
+++ app/src/main/java/tak/phawinee/mytraffic/MainActivity.java
@@ -1,10 +1,17 @@
 package tak.phawinee.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private String urlYoutubeString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +20,18 @@ public class MainActivity extends AppCompatActivity {
     }   //Main Method
 
     public void clickAboutMe(View view) {
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.phonton2);
+        mediaPlayer.start();
+
+
+
+        //Intent to WebView
+        urlYoutubeString = "https://youtu.be/6GfGlGOR9j4";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(urlYoutubeString));
+        startActivity(intent);
+
 
     }
 
